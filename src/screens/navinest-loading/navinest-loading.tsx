b397f572ab1e8b35b5c34576a5a2d28cb2b228be
Arun Sebastian <arunsebastian@gmail.com>
2025-08-11 01:06:37 +1000

    minimal formatting changes

--- src/screens/navinest-loading/navinest-loading.tsx
+++ src/screens/navinest-loading/navinest-loading.tsx
@@ -1,44 +1,54 @@
-import { ThemedActivityIndicator, ThemedFooter, ThemedHeader, ThemedText, ThemedView } from "@/components";
+import {
+    ThemedActivityIndicator,
+    ThemedFooter,
+    ThemedHeader,
+    ThemedText,
+    ThemedView
+} from '@/components';
 import Screens from '@/constants/screens';
 import { useNavigation } from '@react-navigation/native';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import { useEffect, useState } from 'react';
-import { Image } from "react-native";
+import { Image } from 'react-native';
 
 import logoImg from '@assets/images/logo.png';
 
+export const NavinestLoading = () => {
+    const navigation = useNavigation<NativeStackNavigationProp<any>>();
+    const [busy, setBusy] = useState<boolean>(true);
+    const [resourcesLoaded, setResourcesLoaded] = useState<boolean>(true);
 
-export const  NavinestLoading = () => {
-  	const navigation =  useNavigation<NativeStackNavigationProp<any>>();
-	const [busy,setBusy] = useState<boolean>(true);
-	const [resourcesLoaded,setResourcesLoaded] = useState<boolean>(true);
+    useEffect(() => {
+        if (navigation && resourcesLoaded) {
+            const unsubscribe = navigation.addListener('focus', () => {
+                const navigateToKeyIn = async () => {
+                    window.setTimeout(() => {
+                        setBusy(false);
+                        navigation.navigate(Screens.navinestKeyIn);
+                    }, 1000);
+                };
+                navigateToKeyIn();
+            });
+            //Clean up the event listener when the component unmounts
+            return unsubscribe;
+        }
+    }, [resourcesLoaded]);
 
-	useEffect(() => {
-		if (navigation && resourcesLoaded){
-			const unsubscribe = navigation.addListener('focus', () => {
-				const navigateToKeyIn = async () => {
-					window.setTimeout(()=>{
-						setBusy(false)
-						navigation.navigate(Screens.navinestKeyIn)
-					},1000)
-				};
-				navigateToKeyIn();
-			});
-			//Clean up the event listener when the component unmounts
-			return unsubscribe;
-		}
-	}, [resourcesLoaded]);
-
-  return (
-    <ThemedView>
-		<ThemedHeader/>
-		<ThemedView>
-			<Image  source={logoImg} onLoadEnd={() => {setResourcesLoaded(true)}} />
-			<ThemedActivityIndicator animating={busy}/>
-		</ThemedView>
-		<ThemedFooter>
-			<ThemedText type="subtitle">Powered By Navinest</ThemedText>
-		</ThemedFooter>
-    </ThemedView>
-  );
-}
+    return (
+        <ThemedView>
+            <ThemedHeader />
+            <ThemedView>
+                <Image
+                    source={logoImg}
+                    onLoadEnd={() => {
+                        setResourcesLoaded(true);
+                    }}
+                />
+                <ThemedActivityIndicator animating={busy} />
+            </ThemedView>
+            <ThemedFooter>
+                <ThemedText type='subtitle'>Powered By Navinest</ThemedText>
+            </ThemedFooter>
+        </ThemedView>
+    );
+};
