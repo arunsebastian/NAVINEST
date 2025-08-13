be96d46474f4ffa30f57b23ec180614407356ac5
Arun Sebastian <arunsebastian@gmail.com>
2025-08-13 12:33:47 +1000

    code transfer

--- src/screens/navinest-loading/navinest-loading.tsx
+++ src/screens/navinest-loading/navinest-loading.tsx
@@ -13,13 +13,19 @@ import { Image } from 'react-native';
 
 import logoImg from '@assets/images/logo.png';
 
+type LoadtStatus = {
+    success: boolean;
+};
+
 export const NavinestLoading = (props: any) => {
     const navigation = useNavigation<NativeStackNavigationProp<any>>();
     const [busy, setBusy] = useState<boolean>(true);
-    const [resourcesLoaded, setResourcesLoaded] = useState<boolean>(true);
+    const [loadStaus, setLoadStatus] = useState<LoadtStatus>({
+        success: false
+    });
 
     useEffect(() => {
-        if (navigation && resourcesLoaded) {
+        if (navigation && loadStaus.success) {
             console.log(props);
             const unsubscribe = navigation.addListener('focus', () => {
                 const navigateToKeyIn = async () => {
@@ -33,7 +39,7 @@ export const NavinestLoading = (props: any) => {
             //Clean up the event listener when the component unmounts
             return unsubscribe;
         }
-    }, [resourcesLoaded, props]);
+    }, [navigation, loadStaus.success]);
 
     // I AM HERE BELOW
     // I dont think we need the below code seperately - try and integrate on to the effect above with a time out
@@ -49,7 +55,7 @@ export const NavinestLoading = (props: any) => {
                 <Image
                     source={logoImg}
                     onLoadEnd={() => {
-                        setResourcesLoaded(true);
+                        setLoadStatus({ success: true });
                     }}
                 />
                 <ThemedActivityIndicator animating={busy} />
