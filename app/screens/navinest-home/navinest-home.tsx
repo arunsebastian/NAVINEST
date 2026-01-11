81c52dc888770ec91d21f10210723df1aab06e00
Arun Sebastian <arunsebastian@gmail.com>
2026-01-12 02:18:02 +1000

    min items for testing

--- app/screens/navinest-home/navinest-home.tsx
+++ app/screens/navinest-home/navinest-home.tsx
@@ -8,144 +8,58 @@ import {
 import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
-    headerImage: {
-        color: '#808080',
-        bottom: -90,
-        left: -35,
-        position: 'absolute'
-    },
-    titleContainer: {
-        flexDirection: 'row',
-        gap: 8
+    homeItem: {
+        gap: 8,
+        border: '1px solid red',
+        margin: 10
     }
 });
 
 export const NavinestHome = () => {
     return (
         <ThemedScrollView header={<ThemedHeader />} footer={<ThemedFooter />}>
-            <ThemedView style={styles.titleContainer}>
+            <ThemedView style={styles.homeItem}>
                 <ThemedText type='title'>Explore</ThemedText>
+                <ThemedText>
+                    This app includes example code to help you get started.
+                </ThemedText>
             </ThemedView>
-            <ThemedText>
-                This app includes example code to help you get started.
-            </ThemedText>
-            <ThemedView style={styles.titleContainer}>
+            <ThemedView style={styles.homeItem}>
                 <ThemedText type='title'>Explore</ThemedText>
-            </ThemedView>
-            <ThemedText>
-                This app includes example code to help you get started.
-            </ThemedText>
-            <ThemedView style={styles.titleContainer}>
+                <ThemedText>
+                    This app includes example code to help you get started.
+                </ThemedText>
+            </ThemedView>{' '}
+            <ThemedView style={styles.homeItem}>
                 <ThemedText type='title'>Explore</ThemedText>
-            </ThemedView>
-            <ThemedText>
-                This app includes example code to help you get started.
-            </ThemedText>
-            <ThemedView style={styles.titleContainer}>
+                <ThemedText>
+                    This app includes example code to help you get started.
+                </ThemedText>
+            </ThemedView>{' '}
+            <ThemedView style={styles.homeItem}>
                 <ThemedText type='title'>Explore</ThemedText>
-            </ThemedView>
-            <ThemedText>
-                This app includes example code to help you get started.
-            </ThemedText>
-            <ThemedView style={styles.titleContainer}>
+                <ThemedText>
+                    This app includes example code to help you get started.
+                </ThemedText>
+            </ThemedView>{' '}
+            <ThemedView style={styles.homeItem}>
                 <ThemedText type='title'>Explore</ThemedText>
-            </ThemedView>
-            <ThemedText>
-                This app includes example code to help you get started.
-            </ThemedText>
-            <ThemedView style={styles.titleContainer}>
+                <ThemedText>
+                    This app includes example code to help you get started.
+                </ThemedText>
+            </ThemedView>{' '}
+            <ThemedView style={styles.homeItem}>
                 <ThemedText type='title'>Explore</ThemedText>
-            </ThemedView>
-            <ThemedText>
-                This app includes example code to help you get started.
-            </ThemedText>
-            <ThemedView style={styles.titleContainer}>
+                <ThemedText>
+                    This app includes example code to help you get started.
+                </ThemedText>
+            </ThemedView>{' '}
+            <ThemedView style={styles.homeItem}>
                 <ThemedText type='title'>Explore</ThemedText>
+                <ThemedText>
+                    This app includes example code to help you get started.
+                </ThemedText>
             </ThemedView>
-            <ThemedText>
-                This app includes example code to help you get started.
-            </ThemedText>
-            <ThemedView style={styles.titleContainer}>
-                <ThemedText type='title'>Explore</ThemedText>
-            </ThemedView>
-            <ThemedText>
-                This app includes example code to help you get started.
-            </ThemedText>
-            <ThemedView style={styles.titleContainer}>
-                <ThemedText type='title'>Explore</ThemedText>
-            </ThemedView>
-            <ThemedText>
-                This app includes example code to help you get started.
-            </ThemedText>
-            <ThemedView style={styles.titleContainer}>
-                <ThemedText type='title'>Explore</ThemedText>
-            </ThemedView>
-            <ThemedText>
-                This app includes example code to help you get started.
-            </ThemedText>
-            <ThemedView style={styles.titleContainer}>
-                <ThemedText type='title'>Explore</ThemedText>
-            </ThemedView>
-            <ThemedText>
-                This app includes example code to help you get started.
-            </ThemedText>
-            <ThemedView style={styles.titleContainer}>
-                <ThemedText type='title'>Explore</ThemedText>
-            </ThemedView>
-            <ThemedText>
-                This app includes example code to help you get started.
-            </ThemedText>
-            <ThemedView style={styles.titleContainer}>
-                <ThemedText type='title'>Explore</ThemedText>
-            </ThemedView>
-            <ThemedText>
-                This app includes example code to help you get started.
-            </ThemedText>
-            <ThemedText>
-                This app includes example code to help you get started.
-            </ThemedText>
-            <ThemedView style={styles.titleContainer}>
-                <ThemedText type='title'>Explore</ThemedText>
-            </ThemedView>
-            <ThemedText>
-                This app includes example code to help you get started.
-            </ThemedText>
-            <ThemedView style={styles.titleContainer}>
-                <ThemedText type='title'>Explore</ThemedText>
-            </ThemedView>
-            <ThemedText>
-                This app includes example code to help you get started.
-            </ThemedText>
-            <ThemedText>
-                This app includes example code to help you get started.
-            </ThemedText>
-            <ThemedView style={styles.titleContainer}>
-                <ThemedText type='title'>Explore</ThemedText>
-            </ThemedView>
-            <ThemedText>
-                This app includes example code to help you get started.
-            </ThemedText>
-            <ThemedView style={styles.titleContainer}>
-                <ThemedText type='title'>Explore</ThemedText>
-            </ThemedView>
-            <ThemedText>
-                This app includes example code to help you get started.
-            </ThemedText>
-            <ThemedText>
-                This app includes example code to help you get started.
-            </ThemedText>
-            <ThemedView style={styles.titleContainer}>
-                <ThemedText type='title'>Explore</ThemedText>
-            </ThemedView>
-            <ThemedText>
-                This app includes example code to help you get started.
-            </ThemedText>
-            <ThemedView style={styles.titleContainer}>
-                <ThemedText type='title'>Explore</ThemedText>
-            </ThemedView>
-            <ThemedText>
-                This app includes example code to help you get started.
-            </ThemedText>
         </ThemedScrollView>
     );
 };
