c0ad05f437f1f01d01bdc1638fdc91a661ae25a0
Arun Sebastian <arunsebastian@gmail.com>
2026-01-13 19:47:44 +1000

    Navinest Home  placeholder progressed

--- app/components/themed/ThemedScrollView.tsx
+++ app/components/themed/ThemedScrollView.tsx
@@ -1,8 +1,7 @@
 import React, { ReactElement } from 'react';
-import { type ViewProps, StyleSheet } from 'react-native';
+import { type ViewProps, StyleSheet, View } from 'react-native';
 import Animated, { useAnimatedRef } from 'react-native-reanimated';
 
-import { ThemedView } from '@/components/themed/ThemedView';
 import { useThemeColor } from '@/hooks/useThemeColor';
 
 export type ThemedScrollViewProps = ViewProps & {
@@ -26,7 +25,10 @@ const styles = StyleSheet.create({
         height: '100%',
         padding: 20,
         border: '1px solid purple'
-    } as any
+    } as any,
+    scrollViewContent: {
+        flexGrow: 1
+    }
 });
 
 export const ThemedScrollView = ({
@@ -43,16 +45,17 @@ export const ThemedScrollView = ({
     });
     const scrollRef = useAnimatedRef<Animated.ScrollView>();
     return (
-        <ThemedView style={[{ backgroundColor }, styles.view]}>
+        <View style={[{ backgroundColor }, styles.view]}>
             {header ? header : null}
             <Animated.ScrollView
                 ref={scrollRef}
                 scrollEventThrottle={16}
+                contentContainerStyle={styles.scrollViewContent}
                 style={[styles.scrollView, style]}
             >
                 {children}
             </Animated.ScrollView>
             {footer ? footer : null}
-        </ThemedView>
+        </View>
     );
 };
--- app/screens/navinest-home/navinest-home.tsx
+++ app/screens/navinest-home/navinest-home.tsx
@@ -1,75 +1,94 @@
 import {
     ThemedFooter,
     ThemedHeader,
-    ThemedView,
-    ThemedText,
     ThemedScrollView
 } from '@/components/themed';
-import { StyleSheet ,View} from 'react-native';
-import { FlexGrid } from 'react-native-flexible-grid';
-// import { FlatGrid, SectionGrid } from 'react-native-super-grid';
+import { useState } from 'react';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { FlatGrid } from 'react-native-super-grid';
 
 const styles = StyleSheet.create({
-    homeItem: {
-        gap: 8,
-        border: '1px solid red',
-        margin: '10 0 10 0',
-    } as any
+    gridView: {
+        marginTop: 10,
+        flex: 1,
+        display: 'flex'
+    },
+    itemContainer: {
+        justifyContent: 'flex-end',
+        borderRadius: 10,
+        padding: 10,
+        height: 150,
+        cursor: 'pointer'
+    },
+    itemName: {
+        fontSize: 16,
+        color: '#fff',
+        fontWeight: '600'
+    },
+    itemCode: {
+        fontWeight: '600',
+        fontSize: 12,
+        color: '#fff'
+    }
 });
 
 export const NavinestHome = () => {
+    const [items, setItems] = useState([
+        { name: 'TURQUOISE', code: '#1abc9c' },
+        { name: 'EMERALD', code: '#2ecc71' },
+        { name: 'PETER RIVER', code: '#3498db' },
+        { name: 'AMETHYST', code: '#9b59b6' },
+        { name: 'WET ASPHALT', code: '#34495e' },
+        { name: 'GREEN SEA', code: '#16a085' },
+        { name: 'NEPHRITIS', code: '#27ae60' },
+        { name: 'BELIZE HOLE', code: '#2980b9' },
+        { name: 'WISTERIA', code: '#8e44ad' },
+        { name: 'MIDNIGHT BLUE', code: '#2c3e50' },
+        { name: 'SUN FLOWER', code: '#f1c40f' },
+        { name: 'CARROT', code: '#e67e22' },
+        { name: 'ALIZARIN', code: '#e74c3c' },
+        { name: 'CLOUDS', code: '#ecf0f1' },
+        { name: 'CONCRETE', code: '#95a5a6' },
+        { name: 'ORANGE', code: '#f39c12' },
+        { name: 'PUMPKIN', code: '#d35400' },
+        { name: 'POMEGRANATE', code: '#c0392b' },
+        { name: 'SILVER', code: '#bdc3c7' },
+        { name: 'ASBESTOS', code: '#7f8c8d' }
+    ]);
+
+    const renderHomeItem = ({ item }: any) => {
+        return (
+            <TouchableOpacity
+                style={{
+                    flex: 1,
+                    padding: 1
+                }}
+                onPress={() => {
+                    console.log(item.id, 'pressed');
+                }}
+            >
+                <View
+                    style={[
+                        styles.itemContainer,
+                        { backgroundColor: item.code }
+                    ]}
+                >
+                    <Text style={styles.itemName}>{item.name}</Text>
+                    <Text style={styles.itemCode}>{item.code}</Text>
+                </View>
+            </TouchableOpacity>
+        );
+    };
+
     return (
-        <ThemedScrollView  header={<ThemedHeader />} footer={<ThemedFooter />}>
-             <ThemedView style={styles.homeItem}>
-                <ThemedText type='title'>Explore</ThemedText>
-                <ThemedText>
-                    This app includes example code to help you get started.
-                </ThemedText>
-            </ThemedView>
-            {/*
-            // i am here create a GRID layout for the home items and pass the following content as children
-            <ThemedView style={styles.homeItem}>
-                <ThemedText type='title'>Explore</ThemedText>
-                <ThemedText>
-                    This app includes example code to help you get started.
-                </ThemedText>
-            </ThemedView>
-            <ThemedView style={styles.homeItem}>
-                <ThemedText type='title'>Explore</ThemedText>
-                <ThemedText>
-                    This app includes example code to help you get started.
-                </ThemedText>
-            </ThemedView>
-            <ThemedView style={styles.homeItem}>
-                <ThemedText type='title'>Explore</ThemedText>
-                <ThemedText>
-                    This app includes example code to help you get started.
-                </ThemedText>
-            </ThemedView>
-            <ThemedView style={styles.homeItem}>
-                <ThemedText type='title'>Explore</ThemedText>
-                <ThemedText>
-                    This app includes example code to help you get started.
-                </ThemedText>
-            </ThemedView>
-            <ThemedView style={styles.homeItem}>
-                <ThemedText type='title'>Explore</ThemedText>
-                <ThemedText>
-                    This app includes example code to help you get started.
-                </ThemedText>
-            </ThemedView>
-            <ThemedView style={styles.homeItem}>
-                <ThemedText type='title'>Explore</ThemedText>
-                <ThemedText>
-                    This app includes example code to help you get started.
-                </ThemedText>
-            </ThemedView>
-            <ThemedView style={styles.homeItem}>
-                <ThemedText type='title'>Explore</ThemedText>
-                <ThemedText>
-                    This app includes example code to help you get started.
-                </ThemedText>
-            </ThemedView> */}
+        <ThemedScrollView header={<ThemedHeader />} footer={<ThemedFooter />}>
+            <FlatGrid
+                itemDimension={130}
+                data={items}
+                style={styles.gridView}
+                spacing={50}
+                renderItem={renderHomeItem}
+            />
         </ThemedScrollView>
     );
 };
